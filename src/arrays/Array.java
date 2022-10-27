e498adaab710b436e8d34493484673750a8a6789
Burak Ozdemir <ozdemrburak@yahoo.com>
2022-10-27 10:36:30 +0300

    array

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -1,5 +1,20 @@
 package arrays;
 
 public class Array {
-
+	public static void main(String[] args) {
+		int temp,length=10;
+		int[] array = {12,87,45,61,9,7,45,78,56,31};
+		System.out.printf("%s%8s%n","Index","Value");
+		for (int counter =0; counter<length/2;counter++) {
+			temp=array[counter];
+			array[counter]=array[length-1-counter];
+			array[length-1-counter]=temp;
+		}
+		for (int counter =0; counter<length;counter++) {
+			System.out.printf("%5d%8d%n",counter,array[counter]);
+		}
+		
+		
+		
+	}
 }
